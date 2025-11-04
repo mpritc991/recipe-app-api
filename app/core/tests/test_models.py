09f808111ff7114123d68edc10fc184d9d52e6cc
mpritc991 <mpritc991@gmail.com>
2025-11-03 18:10:18 -0600

    saving edited version,  and running the test_models.py file

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -17,5 +17,6 @@ class ModelTests(TestCase):
             email=email,
             password=password
         )
+
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
