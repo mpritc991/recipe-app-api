9dc97b9d4d1756b45529298ef066c39f0663e5b9
mpritc991 <mpritc991@gmail.com>
2025-11-07 12:33:33 -0600

    committing for the tests and models updates

--- app/core/models.py
+++ app/core/models.py
@@ -7,13 +7,22 @@ from django.contrib.auth.models import (
     BaseUserManager,
     PermissionsMixin,
 )
+
 # Create your models here.
+
+#my code looks different, but for reference, my instructors code looks like this:
+
+#    def create_user(self, email, password=None, **extra_fields):
+#        user = self.model(email=self.normalize_email(email), **extra_fields)
+#        user.set_password(password)
+#        user.save(using=self._db)
+
 
 class UserManager(BaseUserManager):
     """Manager for users."""
 
     def create_user(self, email, password=None, **extra_fields):
-        """Create, save and return a regular user with the given email and password."""
+        """Create and save a regular user with the given email and password."""
         if not email:
             raise ValueError('Users must have an email address')
         email = self.normalize_email(email)
@@ -24,18 +33,23 @@ class UserManager(BaseUserManager):
         return user
 
 #    def create_superuser(self, email, password):
-#        """Create and save a superuser with the given email and password."""
-#        user = self.create_user(email, password)
-#        user.is_staff = True
-#        user.is_superuser = True
-#        user.save(using=self._db)
-
-#        return user
+#       """Create and save a superuser with
+#       the given email and password.
+#       """
+#       user = self.create_user(email, password)
+#       user.is_staff = True
+#       user.is_superuser = True
+#       user.save(using=self._db)
+#       return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that supports using email instead of username."""
+    """
+    Custom user model that supports
+    using email instead of username.
+    """
     email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -24,7 +24,18 @@ class ModelTests(TestCase):
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized."""
         sample_emails = [
-            # anything before @ can remain uppercase, domain part should be lowercased.
-            ['testone@EXAMPLE.COM', 'testone@example.com'],
-            ['Test2@Example.com', 'Test2@example.com']
+            # anything before @ can remain uppper,
+            # domain part should be lowercased
+            ['test1@EXAMPLE.COM', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.COM', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com'],
         ]
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, 'sample123')
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
