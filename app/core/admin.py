429efe8e5e5865177b504349436b93eddadea89e
mpritc991 <mpritc991@gmail.com>
2025-11-13 14:09:54 -0600

    standard commit to test on

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,44 @@
-from django.contrib import admin
+"""
+django admin customizations"""
 
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 # Register your models here.
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None,
+        {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'),
+         {'fields': ('last_login',)})
+    )
+    readonly_fields = ['last_login']
+#   
+#         ('Personal Info', {'fields': ('name',)}),
+#         (
+#             'Permissions',
+#             {'fields': ('is_active', 'is_staff', 'is_superuser')}
+#         ),
+#         ('Important dates', {'fields': ('last_login',)}),
+#     )
+#     add_fieldsets = (
+#         (None, {
+#             'classes': ('wide',),
+#             'fields': ('email', 'password1', 'password2')
+#         }),
+#     )
+admin.site.register(models.User, UserAdmin)
