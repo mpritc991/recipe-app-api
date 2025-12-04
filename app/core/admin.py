7e933831dddf1ecb4d261b6f9e8ce2dab39cc38f
mpritc991 <mpritc991@gmail.com>
2025-12-04 17:16:10 -0600

    just a commit #3

--- app/core/admin.py
+++ app/core/admin.py
@@ -7,20 +7,20 @@ from django.utils.translation import gettext_lazy as _
 # Register your models here.
 from core import models
 
+
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
-        (None,
-        {'fields': ('email', 'password')}),
+        (None, {'fields': ('email', 'password')}),
         (
             _('Permissions'),
             {
                 'fields': (
-                'is_active',
-                'is_staff',
-                'is_superuser',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
                 )
             }
         ),
