763a1b298d77e8949af3e4af07d0b8640c998477
mpritc991 <mpritc991@gmail.com>
2025-12-09 17:14:32 -0600

    todays commit

---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,29 @@
+"""
+Test cases for recipe API endpoints.
+"""
+
+from decimal import Decimal
+
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Recipe
+
+from recipe.serializers import RecipeSerializer
+#        **params  is a "dictionary" of parameters
+def create_recipe(user, **params):
+    """Helper function to create and return a sample recipe."""
+    defaults = {
+        'title': 'Sample recipe',
+        'time_minutes': 10,
+        'price': Decimal('5.00'),
+        'description': 'Sample description',
+        'link': 'http://example.com/recipe.pdf',
+    }
+    defaults.update(params)
+    recipe = Recipe.objects.create(user=user, **defaults)
+    return recipe
